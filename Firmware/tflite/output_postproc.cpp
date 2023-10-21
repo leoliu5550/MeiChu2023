2d5c2286f610edc6b8a3afc643afad3dc18e91b2
William Chang <wilicw1346@gmail.com>
2023-10-21 21:11:02 +0800

    feat: output index and confidence

--- Firmware/tflite/output_postproc.cpp
+++ Firmware/tflite/output_postproc.cpp
@@ -15,7 +15,7 @@
 #endif
 
 status_t MODEL_ProcessOutput(const uint8_t* data, const tensor_dims_t* dims,
-                             tensor_type_t type, int inferenceTime)
+                             tensor_type_t type, int inferenceTime, int* r_idnex, int* r_confidence)
 {
     const float threshold = (float)DETECTION_TRESHOLD / 100;
     result_t topResults[NUM_RESULTS];
@@ -33,14 +33,16 @@ status_t MODEL_ProcessOutput(const uint8_t* data, const tensor_dims_t* dims,
         if (confidence * 100 > DETECTION_TRESHOLD)
         {
             label = labels[index];
+            *r_idnex = index;
         }
     }
 
     int score = (int)(confidence * 100);
-    PRINTF("----------------------------------------" EOL);
-    PRINTF("     Inference time: %d ms" EOL, inferenceTime / 1000);
-    PRINTF("     Detected: %s (%d%%)" EOL, label, score);
-    PRINTF("----------------------------------------" EOL);
+    *r_confidence = score;
+    // PRINTF("----------------------------------------" EOL);
+    // PRINTF("     Inference time: %d ms" EOL, inferenceTime / 1000);
+    // PRINTF(":::Detected: %s (%d%%)" EOL, label, score);
+    // PRINTF("----------------------------------------" EOL);
 
 #ifdef EIQ_GUI_PRINTF
     GUI_PrintfToBuffer(GUI_X_POS, GUI_Y_POS, "Detected: %.20s (%d%%)", label, score);
